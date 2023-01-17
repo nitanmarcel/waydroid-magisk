4eb6ac0309d939009aee51d632d3d6bbf96f833e
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-01-17 12:11:58 +0200

    debian: version 1.0.3

--- waydroid_magisk.py
+++ waydroid_magisk.py
@@ -31,7 +31,7 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt="%H:%M:%S")
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 MAGISK_HOST = "https://huskydg.github.io/magisk-files/"
 MAGISK_CANARY = "%s/app-release.apk" % MAGISK_HOST
