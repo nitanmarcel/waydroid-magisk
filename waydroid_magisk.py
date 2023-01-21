d5a6200d248eccb75503bb2ac608a110d15a84b5
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-01-21 23:08:05 +0200

    waydroid_magisk: version 1.2.5

--- waydroid_magisk.py
+++ waydroid_magisk.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt="%H:%M:%S")
 
-VERSION = '1.2.3'
+VERSION = '1.2.5'
 
 MAGISK_HOST = "https://huskydg.github.io/magisk-files/"
 MAGISK_CANARY = "%s/app-release.apk" % MAGISK_HOST
